949189a9afad76a15e69992aef37b9a7af80fe5a
MohanSai-S <sathyanarayanamohansai.gmail.com>
2021-04-27 12:57:14 +0530

    Added : Employee wage caluclator per month.

--- src/EmployeeWageBuilder/EmpWageBuilder.java
+++ src/EmployeeWageBuilder/EmpWageBuilder.java
@@ -1,34 +1,39 @@
 package EmployeeWageBuilder;
 
 public class EmpWageBuilder {
-
+	
+	//Constants
+	public static final int isFullTime=1;
+	public static final int isPartTime=2;
+	public static final int wagePerHour=20;
+	public static final int noOfWorkingDays=20;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Employee Wage");
-		//Constants
-		final int isFullTime=1;
-		final int isPartTime=2;
-		final int wagePerHour=20;
 		//Variables
+		int i=0;
 		int workingHours=0;
+		int totalWorkingHours=0;
+		int empWage=0;
 		//Computation		
-		double random = Math.floor(Math.random()*10)%3;
-		int empCheck =(int) Math.round(random);
-		switch (empCheck) {
 		
-		case isFullTime:
-			workingHours=8;
-			System.out.println("Employee is Full time");
-			break;
-		case isPartTime:
-			workingHours=4;
-			System.out.println("Employee is Part time");
-			break;
-		default:
-			workingHours=0;
-			System.out.println("Employee is Absent");
+		for (i=0;i<noOfWorkingDays;i++) {
+			
+			int empCheck =(int) Math.round(Math.floor(Math.random()*10)%3);
+		
+			switch (empCheck) {
+			
+			case isFullTime:
+				workingHours=8;
+				break;
+			case isPartTime:
+				workingHours=4;
+				break;
+			default:
+				workingHours=0;
+			}
+			totalWorkingHours=totalWorkingHours+workingHours;
 		}
-		int wage = wagePerHour*workingHours;
-		System.out.println("Employee wage per day is "+ wage);
+		empWage = wagePerHour*totalWorkingHours;
+		System.out.println("Employee wage per month is "+ empWage);
 	}
 }
