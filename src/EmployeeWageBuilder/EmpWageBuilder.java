14103824315277b33e25e4e01710dd44c7dec68c
MohanSai-S <sathyanarayanamohansai.gmail.com>
2021-04-27 12:23:17 +0530

    Added : Employee wage builder using Switch case

--- src/EmployeeWageBuilder/EmpWageBuilder.java
+++ src/EmployeeWageBuilder/EmpWageBuilder.java
@@ -6,21 +6,25 @@ public class EmpWageBuilder {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage");
 		//Constants
-		int isFullTime=1;
-		int isPartTime=2;
+		final int isFullTime=1;
+		final int isPartTime=2;
+		final int wagePerHour=20;
+		//Variables
 		int workingHours=0;
-		int wagePerHour=20;
 		//Computation		
-		double empCheck = Math.floor(Math.random()*10)%3;
-		if (empCheck == isFullTime) {
+		double random = Math.floor(Math.random()*10)%3;
+		int empCheck =(int) Math.round(random);
+		switch (empCheck) {
+		
+		case isFullTime:
 			workingHours=8;
 			System.out.println("Employee is Full time");
-		}
-		else if (empCheck == isPartTime) {
+			break;
+		case isPartTime:
 			workingHours=4;
 			System.out.println("Employee is Part time");
-		}
-		else {
+			break;
+		default:
 			workingHours=0;
 			System.out.println("Employee is Absent");
 		}
