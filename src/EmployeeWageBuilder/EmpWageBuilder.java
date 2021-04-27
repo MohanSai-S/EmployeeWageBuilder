3647203d40ac72ad4291d6ae7edd6fd10f7f1eea
MohanSai-S <sathyanarayanamohansai.gmail.com>
2021-04-27 19:34:14 +0530

    Added : Emplyee Wage caluclator according to particular time or days

--- src/EmployeeWageBuilder/EmpWageBuilder.java
+++ src/EmployeeWageBuilder/EmpWageBuilder.java
@@ -7,32 +7,40 @@ public class EmpWageBuilder {
 	public static final int isPartTime=2;
 	public static final int wagePerHour=20;
 	public static final int noOfWorkingDays=20;
+	public static final int maxWorkingHours=100;
 	
 	public static void main(String[] args) {
 		//Variables
-		int i=0;
+		int day=0;
 		int workingHours=0;
 		int totalWorkingHours=0;
 		int empWage=0;
 		//Computation		
 		
-		for (i=0;i<noOfWorkingDays;i++) {
+		for (day=1;day<=noOfWorkingDays;day++) {
 			
 			int empCheck =(int) Math.round(Math.floor(Math.random()*10)%3);
-		
-			switch (empCheck) {
 			
-			case isFullTime:
-				workingHours=8;
-				break;
-			case isPartTime:
-				workingHours=4;
-				break;
-			default:
-				workingHours=0;
-			}
-			totalWorkingHours=totalWorkingHours+workingHours;
+			if (day<=noOfWorkingDays && totalWorkingHours<=maxWorkingHours) {
+		
+				switch (empCheck) {
+				
+				case isFullTime:
+					workingHours=8;
+					break;
+				case isPartTime:
+					workingHours=4;
+					break;
+				default:
+					workingHours=0;
+				}
+				totalWorkingHours += workingHours;
+			}			
 		}
+		if (totalWorkingHours>100) {
+			totalWorkingHours -= workingHours;
+		}
+		System.out.println(" Number of Working Hours " + totalWorkingHours);
 		empWage = wagePerHour*totalWorkingHours;
 		System.out.println("Employee wage per month is "+ empWage);
 	}
