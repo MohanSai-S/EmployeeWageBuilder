7f614719100a63e29d1c27c062d73afd1ff0e098
MohanSai-S <sathyanarayanamohansai.gmail.com>
2021-04-30 09:44:33 +0530

    Refactored : Refactored code from Procedural to OOPs

--- src/EmployeeWageBuilder/EmpWageBuilder.java
+++ src/EmployeeWageBuilder/EmpWageBuilder.java
@@ -8,40 +8,57 @@ public class EmpWageBuilder {
 	public static final int wagePerHour=20;
 	public static final int noOfWorkingDays=20;
 	public static final int maxWorkingHours=100;
+	int workingHours=0;
 	
-	public static void main(String[] args) {
+	void attendance() {
+		//Computation
+		int empCheck =(int) Math.round(Math.floor(Math.random()*10)%3);
+	
+		switch (empCheck) {
+	
+			case isFullTime:
+				workingHours=8;
+				break;
+			case isPartTime:
+				workingHours=4;
+				break;
+			default:
+				workingHours=0;
+	}
+		
+	}
+		
+	void wageComputation(String employeeName) {
+		
 		//Variables
 		int day=0;
-		int workingHours=0;
 		int totalWorkingHours=0;
 		int empWage=0;
 		//Computation		
 		
 		for (day=1;day<=noOfWorkingDays;day++) {
 			
-			int empCheck =(int) Math.round(Math.floor(Math.random()*10)%3);
+			
 			
 			if (day<=noOfWorkingDays && totalWorkingHours<=maxWorkingHours) {
 		
-				switch (empCheck) {
-				
-				case isFullTime:
-					workingHours=8;
-					break;
-				case isPartTime:
-					workingHours=4;
-					break;
-				default:
-					workingHours=0;
-				}
+				attendance();
 				totalWorkingHours += workingHours;
 			}			
 		}
-		if (totalWorkingHours>100) {
+		if (totalWorkingHours>maxWorkingHours) {
 			totalWorkingHours -= workingHours;
 		}
-		System.out.println(" Number of Working Hours " + totalWorkingHours);
+		System.out.println("Salary Details of "+ employeeName);
+		System.out.println(" Number of Working Hours : " + totalWorkingHours);
 		empWage = wagePerHour*totalWorkingHours;
-		System.out.println("Employee wage per month is "+ empWage);
+		System.out.println("Employee wage per month : "+ empWage);
+	
+		}
+	public static void main(String[] args) {
+		EmpWageBuilder Employee1= new EmpWageBuilder();
+		EmpWageBuilder Employee2= new EmpWageBuilder();
+		Employee1.wageComputation("Employee1");
+		Employee2.wageComputation("Employee2");
 	}
 }
